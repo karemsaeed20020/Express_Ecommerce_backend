3c6997c0beef6a2eac598d2ce9c31ff6830557c7
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2024-09-04 18:00:49 +0300

    Create POST add category controller and routes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dbConnection from './databases/dbConnection.js';
+import bootstrap from './src/bootstrap.js';
 const app = express();
 
 const port = 5000;
-
+app.use(express.json());
+bootstrap(app);
 dbConnection();
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
