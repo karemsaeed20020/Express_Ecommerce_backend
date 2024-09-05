06f41001c047dfd05ea53759051c753870059bdd
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2024-09-05 15:14:46 +0300

    Category CRUD (getAllCategories and updateCategory and deleteCategory)

--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -6,3 +6,22 @@ export const addCategory = async (req, res) => {
     await category.save();
     res.status(201).json({message: "Success", category})
 }
+
+export const getAllCategories = async (req, res) => {
+    let categories = await categoryModel.find();
+    res.status(200).json({message: "Success", categories});
+}
+export const updateCategory = async (req, res) => {
+    const {id} = req.params;
+    const {name} = req.body;
+    req.body.slug = slugify(req.body.name);
+    let category = await categoryModel.findByIdAndUpdate(id, req.body, {new: true});
+    !category && res.status(404).json({message: "Category not found"});
+    category && res.status(200).json({message: "Success",category});
+}
+export const deleteCategory = async (req, res) => {
+    const {id} = req.params;
+    let category = await categoryModel.findByIdAndDelete(id);
+    !category && res.status(404).json({message: "Category not found"});
+    category && res.status(200).json({message: "Success",category});
+}
--- src/modules/category/category.routes.js
+++ src/modules/category/category.routes.js
@@ -2,6 +2,8 @@ import express from 'express';
 import * as categoryController from './category.controller.js';
 const categoryRouter = express.Router();
 
-categoryRouter.post("/",categoryController.addCategory);
+categoryRouter.route("/").post(categoryController.addCategory).get(categoryController.getAllCategories)
+
+categoryRouter.route("/:id").put(categoryController.updateCategory).delete(categoryController.deleteCategory)
 
 export default categoryRouter;
