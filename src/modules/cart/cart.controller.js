4d1dc7edd9f1e48289029d9c1fbeff1aad479497
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2024-09-13 23:41:28 +0300

    RemoveFromCart and update product quantity and getLoggedUserCart

--- src/modules/cart/cart.controller.js
+++ src/modules/cart/cart.controller.js
@@ -1,4 +1,5 @@
 import cartModel from "../../../databases/models/cart.model.js";
+import couponModel from "../../../databases/models/coupon.model.js";
 import productModel from "../../../databases/models/product.model.js";
 import { AppError } from "../../utils/AppError.js";
 import { catchError } from "../../utils/catchError.js";
@@ -65,3 +66,48 @@ export const addProductToCart = catchError(async (req, res, next) => {
     .status(201)
     .json({ message: "Product added to cart", cart: isCartExists });
 });
+export const removeProductFromCart = catchError(async(req, res, next) => {
+  let result = await cartModel.findOneAndUpdate({user: req.user._id}, {$pull: {cartItems: {_id: req.params.id}}});
+  !result && next(new AppError("Cart not found", 404));
+  calcTotalPrice(result);
+  result && res.status(200).json({message: "Success", cart: result});
+})
+export const updateQuantity = catchError(async (req, res, next) => {
+  let product = await productModel.findById(req.params.id).select("price");
+  if (!product) {
+    return next(new AppError("Product not found", 404));
+  }
+
+ 
+
+  let isCartExists = await cartModel.findOne({ user: req.user._id });
+
+  
+  let item = isCartExists.cartItems.find(
+    (elm) => elm.product.toString() === req.params.id
+  );
+
+  if (item) {
+    item.quantity = req.body.quantity;
+  } 
+  
+
+  calcTotalPrice(isCartExists);
+
+  await isCartExists.save();
+
+  return res
+    .status(201)
+    .json({ message: "Success", cart: isCartExists });
+});
+export const getLoggedUserCart = catchError(async(req, res, next) => {
+  let cartItems = await cartModel.findOne({user: req.user._id}).populate("cartItems.product");
+  res.status(201).json({message: "Success", cart: cartItems});
+})
+export const applyCoupon = catchError(async(req, res, next) => {
+  let coupon = await couponModel.findOne({code: req.body.code, expires: {$gt: date.now()}});
+  let cart = await cartModel.findOne({use:req.user._id});
+  cart.totalPriceAfterDiscount = cart.totalPrice - (cart.totalPrice * coupon.discount) / 100;
+  await cart.save();
+  res.status(200).json({message: "Success!", cart});
+})
--- src/modules/cart/cart.routes.js
+++ src/modules/cart/cart.routes.js
@@ -3,6 +3,7 @@ import { allowedTo, protectedRoutes } from '../auth/auth.controller.js';
 import * as cartController from './cart.controller.js'
 const cartRouter = express.Router();
 
-cartRouter.route('/').post(protectedRoutes, allowedTo("user"), cartController.addProductToCart);
+cartRouter.route('/').post(protectedRoutes, allowedTo("user"), cartController.addProductToCart).get(protectedRoutes, allowedTo("user"), cartController.getLoggedUserCart);
+cartRouter.route('/:id').delete(protectedRoutes, allowedTo("user"), cartController.removeProductFromCart).put(protectedRoutes, allowedTo("user"), cartController.updateQuantity);
 
 export default cartRouter;
